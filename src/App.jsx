9a3ab2000ea5208f30a8452615b7255d95ef7dad
RohanGorla <rohangorla2010@gmail.com>
2024-08-05 21:46:41 +0530

    add react components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
+import Projects from "./Components/Projects";
+import About from "./Components/About";
+import Footer from "./Components/Footer";
 
 function App() {
-
   return (
     <>
-      <h1>This is my portfolio</h1>      
+      <Navbar />
+      <Home />
+      <Projects />
+      <About />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
