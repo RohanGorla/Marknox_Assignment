a0f720761c29c332f0c05e090a542fd1a01b0964
RohanGorla <rohangorla2010@gmail.com>
2024-08-05 21:20:47 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>This is my portfolio</h1>      
+    </>
+  )
+}
+
+export default App
